70c96b5ccff676377cc790492d2ed5d3fa526c67
goeun23 <abgoeun3339@gmail.com>
2023-05-27 22:45:42 +0900

    로그인 페이지 추가

--- src/main.js
+++ src/main.js
@@ -7,11 +7,13 @@ import * as VueRouter from 'vue-router'
 
 import Admin from './views/admin.vue' 
 import Commerce from './views/commerce.vue' 
+import Login from './views/login.vue' 
 
 const routes = [
     { path: '/', component: Admin }, 
     { path: '/admin', component: Admin }, 
     { path: '/commerce', component: Commerce },
+    { path: '/login', component: Login }, 
  ]
 
 const router = VueRouter.createRouter({
