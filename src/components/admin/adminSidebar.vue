ca033c084a09f7978b045c20dd40a0c640866d60
goeun23 <abgoeun3339@gmail.com>
2023-06-18 18:55:27 +0900

    commit

--- src/components/admin/adminSidebar.vue
+++ src/components/admin/adminSidebar.vue
@@ -65,7 +65,7 @@
                 </router-link>
               </li>
               <li>
-                <router-link  to="/admin/product/template" class="flex items-center gap-x-3.5 py-2 px-2.5 text-base text-slate-700 rounded-md hover:bg-gray-100 dark:bg-gray-800 dark:text-slate-400 dark:hover:text-slate-300" href="javascript:;">
+                <router-link  to="/admin/template" class="flex items-center gap-x-3.5 py-2 px-2.5 text-base text-slate-700 rounded-md hover:bg-gray-100 dark:bg-gray-800 dark:text-slate-400 dark:hover:text-slate-300" href="javascript:;">
                   팜플렛 만들기
                 </router-link>
               </li>
