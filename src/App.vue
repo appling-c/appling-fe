a1a3315e88422015d825276b43fbe7f7331266d3
goeun23 <abgoeun3339@gmail.com>
2023-11-19 14:23:26 +0900

    주문상세 UI

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,4 @@
 <script>
-<<<<<<< HEAD
-	import { mapGetters } from "vuex";
-	export default {
-		computed: {
-			...mapGetters("cart", ["spinnerStatus"]),
-		},
-	};
-</script>
-
-<template>
-	<div>
-		<router-view />
-		<template v-if="spinnerStatus">
-			<div
-				class="absolute top-0 left-0 w-full h-full bg-white/[.5] rounded-md dark:bg-gray-800/[.4]"
-			></div>
-			<div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-				<div
-					class="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full"
-					role="status"
-					aria-label="loading"
-				>
-					<span class="sr-only">Loading...</span>
-				</div>
-			</div>
-		</template>
-		<footer class="mt-auto w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
-			<!-- Grid -->
-			<div class="text-center">
-				<div>
-					<a
-						class="flex-none text-base font-semibold text-black dark:text-white"
-						aria-label="Brand"
-						>🍎Appling</a
-					>
-				</div>
-				<!-- End Col -->
-=======
 import { mapGetters } from "vuex";
 import { initFlowbite } from "flowbite";
 export default {
@@ -77,7 +39,6 @@ export default {
           >
         </div>
         <!-- End Col -->
->>>>>>> 86344d1c86c4eb717846bde51861cf0161f78033
 
 				<div class="mt-3">
 					<p class="text-gray-500">© Copyright 2023. Appling. All rights reserved.</p>
