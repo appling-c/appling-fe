09cd724557f14be00a0f76021927c7f7381ac898
luna.n <luna.n@kakaovx.com>
2023-11-27 08:20:15 +0900

    axios 토큰 리프레시 오류 수정

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -4,66 +4,71 @@ import store from "../store";
 import Cookies from "js-cookie";
 
 let instance: AxiosInstance = axios.create({
-  //baseURL: import.meta.env.VITE_APP_BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+	//baseURL: import.meta.env.VITE_APP_BASE_URL,
+	headers: {
+		"Content-Type": "application/json",
+	},
 });
 
 if (import.meta.env.PROD) {
-  instance.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL;
+	instance.defaults.baseURL = import.meta.env.VITE_APP_BASE_URL;
 }
 
 instance.interceptors.request.use(
-  (config) => {
-    // 세션스토리지에 저장된 토큰을 헤더에 저장
+	(config) => {
+		// 세션스토리지에 저장된 토큰을 헤더에 저장
 
-    const access_token = Cookies.get("access_token");
+		const access_token = Cookies.get("access_token");
 
-    if (access_token) {
-      config.headers["Authorization"] = `Bearer ${access_token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+		if (access_token) {
+			config.headers["Authorization"] = `Bearer ${access_token}`;
+		}
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
 );
 
 instance.interceptors.response.use(
-  (response) => {
-    console.log("axios.js response : ", response);
-    return response;
-  },
-  (error) => {
-    const {
-      config,
-      response: { status },
-    } = error;
+	(response) => {
+		console.log("axios.js response : ", response);
+		return response;
+	},
+	(error) => {
+		const {
+			config,
+			response: { status },
+		} = error;
 
-    const refresh_token = Cookies.get("refresh_token");
+		const refresh_token = Cookies.get("refresh_token");
 
-    if (status == 403 || status == 401) {
-      // 토큰없음, 토큰만료
-      const originconfig = config;
+		// 토큰이 없고, 리프레시 토큰도 없는 경우 > 로그인 만료
+		if (!refresh_token) {
+			alert("사용자 정보가 만료되었습니다. 다시 로그인해주세요.");
+			location.href = `/login`;
 
-      // 토큰이 없고, 리프레시 토큰도 없는 경우 > 로그인 만료
-      if (!refresh_token) {
-        alert("사용자 정보가 만료되었습니다. 다시 로그인해주세요.");
-        location.href = `/login`;
+			return UserAthendicateService.logout();
+		}
 
-        return UserAthendicateService.logout();
-      }
+		if (status == 403 || status == 401) {
+			// 토큰없음, 토큰만료
+			const originconfig = config;
 
-      // 토큰 재발급 요청
-      return UserAthendicateService.memberaccesstoken().then((response) => {
-        console.log("토큰 재발급 요청");
-        const retry = instance(originconfig);
-        return retry;
-      });
-    } else {
-      return Promise.reject(error);
-    }
-  }
+			// 토큰 재발급 요청
+			UserAthendicateService.memberaccesstoken().then((response) => {
+				console.log("토큰 재발급 요청", response);
+				if (response == "fail") {
+					alert("사용자 정보가 만료되었습니다. 다시 로그인해주세요.");
+					location.href = `/login`;
+					return UserAthendicateService.logout();
+				} else {
+					return instance(originconfig);
+				}
+			});
+		} else {
+			return Promise.reject(error);
+		}
+	}
 );
 export default instance;
--- src/services/UserAthendicateService.ts
+++ src/services/UserAthendicateService.ts
@@ -3,152 +3,160 @@ import { AxiosHeaders, AxiosResponse } from "axios";
 import { Token, MemberLogin } from "../types/auth";
 import store from "../store";
 import Cookies from "js-cookie";
+
 const ENDPOINT = {
-  KAKAOLOGIN_AUTH: "/api/oauth/kakao?code=",
-  KAKAOLOGIN_AUTH_KAKAO_TOKEN: "/api/oauth/kakao/login?access_token=",
-  API_AUTH_LOGIN: "/api/auth/login",
-  API_AUTH_REFRESH: "/api/auth/refresh",
-  API_AUTH_JOIN: "/api/auth/join",
-  API_MEMEBER: "/api/member",
+	KAKAOLOGIN_AUTH: "/api/oauth/kakao?code=",
+	KAKAOLOGIN_AUTH_KAKAO_TOKEN: "/api/oauth/kakao/login?access_token=",
+	API_AUTH_LOGIN: "/api/auth/login",
+	API_AUTH_REFRESH: "/api/auth/refresh",
+	API_AUTH_JOIN: "/api/auth/join",
+	API_MEMEBER: "/api/member",
 };
 
 class UserAthendicateService {
-  /**
-   * 회원정보수정
-   */
-  public async updatememberinfo(payload: Object): Promise<any> {
-    return await instance.patch(ENDPOINT.API_MEMEBER, { ...payload }).then((response) => {
-      return response;
-    });
-  }
+	/**
+	 * 회원정보수정
+	 */
+	public async updatememberinfo(payload: Object): Promise<any> {
+		return await instance.patch(ENDPOINT.API_MEMEBER, { ...payload }).then((response) => {
+			return response;
+		});
+	}
 
-  /**
-   * 로그아웃
-   */
-  logout(): void {
-    store.dispatch("auth/updateUserInfoInterface", {});
-    Cookies.remove("access_token");
-    Cookies.remove("refresh_token");
-  }
+	/**
+	 * 로그아웃
+	 */
+	logout(): void {
+		store.dispatch("auth/updateUserInfoInterface", {});
+		Cookies.remove("access_token");
+		Cookies.remove("refresh_token");
+	}
 
-  /**
-   * 회원가입
-   */
-  public async memberjoin(payload): Promise<void> {
-    await instance.post(ENDPOINT.API_AUTH_JOIN, { ...payload }).then((response) => {
-      const { code } = response.data;
-      if (code == "0000") {
-        return "login";
-      } else {
-        return "signup";
-      }
-    });
-  }
+	/**
+	 * 회원가입
+	 */
+	public async memberjoin(payload): Promise<void> {
+		await instance.post(ENDPOINT.API_AUTH_JOIN, { ...payload }).then((response) => {
+			const { code } = response.data;
+			if (code == "0000") {
+				return "login";
+			} else {
+				return "signup";
+			}
+		});
+	}
 
-  /**
-   * 토큰재발급
-   */
-  public async memberaccesstoken(): Promise<string> {
-    const refresh_token = Cookies.get("refresh_token");
-    await instance.get(`${ENDPOINT.API_AUTH_REFRESH}/${refresh_token}`).then((response) => {
-      const { code, data } = response.data;
-      if (code == "0000") {
-        // 토큰저장
-        this._save_token(data);
-      }
-    });
+	/**
+	 * 토큰재발급
+	 */
+	public async memberaccesstoken(): Promise<string> {
+		const refresh_token = Cookies.get("refresh_token");
+		try {
+			await instance.get(`${ENDPOINT.API_AUTH_REFRESH}/${refresh_token}`).then((response) => {
+				const { code, data } = response.data;
+				if (code == "0000") {
+					// 토큰저장
+					this._save_token(data);
+				}
+				return new Promise((resolve) => {
+					setTimeout(() => {
+						resolve("success");
+					}, 500);
+				});
+			});
+		} catch (err) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve("fail");
+        }, 500);
+      })
+		}
+	}
 
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve("success");
-      }, 500);
-    });
-  }
+	/**
+	 * user token 발행, 토큰저장
+	 */
+	public async loginccesstoken(payload: Object): Promise<void> {
+		await instance.post(ENDPOINT.API_AUTH_LOGIN, { ...payload }).then((response) => {
+			const { code, data } = response.data;
+			if (code == "0000") {
+				this._save_token(data);
+			}
+		});
+	}
 
-  /**
-   * user token 발행, 토큰저장
-   */
-  public async loginccesstoken(payload: Object): Promise<void> {
-    await instance.post(ENDPOINT.API_AUTH_LOGIN, { ...payload }).then((response) => {
-      const { code, data } = response.data;
-      if (code == "0000") {
-        this._save_token(data);
-      }
-    });
-  }
+	/**
+	 * 카카오 로그인 리다이렉팅
+	 */
+	public async kakaologin(code: string) {
+		let kakao_access_token: string;
+		await instance.get(`${ENDPOINT.KAKAOLOGIN_AUTH}${code}`).then((response: AxiosResponse) => {
+			const { data, code } = response?.data;
+			if (code == "0000") {
+				const { access_token }: Token = data;
+				kakao_access_token = access_token;
+			}
+		});
 
-  /**
-   * 카카오 로그인 리다이렉팅
-   */
-  public async kakaologin(code: string) {
-    let kakao_access_token: string;
-    await instance.get(`${ENDPOINT.KAKAOLOGIN_AUTH}${code}`).then((response: AxiosResponse) => {
-      const { data, code } = response?.data;
-      if (code == "0000") {
-        const { access_token }: Token = data;
-        kakao_access_token = access_token;
-      }
-    });
+		await instance
+			.get(`${ENDPOINT.KAKAOLOGIN_AUTH_KAKAO_TOKEN}${kakao_access_token}`)
+			.then((auth: AxiosResponse) => {
+				//const data = auth.data;
+				const data = auth.data.data;
+				this._save_token(data);
+			});
 
-    await instance
-      .get(`${ENDPOINT.KAKAOLOGIN_AUTH_KAKAO_TOKEN}${kakao_access_token}`)
-      .then((auth: AxiosResponse) => {
-        //const data = auth.data;
-        const data = auth.data.data;
-        this._save_token(data);
-      });
+		return await this.memberinfo().then((resultpath: string) => {
+			return resultpath;
+		});
+	}
 
-    return await this.memberinfo().then((resultpath: string) => {
-      return resultpath;
-    });
-  }
+	/**
+	 * 토큰저장
+	 */
+	_save_token(payload: Token): void {
+		// 토큰저장
+		const { access_token, refresh_token }: Token = payload;
+		store.dispatch("auth/saveUserToken", {
+			access_token,
+			refresh_token,
+		});
+	}
 
-  /**
-   * 토큰저장
-   */
-  _save_token(payload: Token): void {
-    // 토큰저장
-    const { access_token, refresh_token }: Token = payload;
-    store.dispatch("auth/saveUserToken", {
-      access_token,
-      refresh_token,
-    });
-  }
+	/**
+	 * 사용자 정보 가져오기
+	 */
+	public async memberinfo(): Promise<string> {
+		let resultpath: string;
+		await instance.get(ENDPOINT.API_MEMEBER).then((result) => {
+			let { data, message } = result.data;
+			// store 저장
+			data["islogin"] = true;
+			store.dispatch("auth/updateUserInfoInterface", data);
 
-  /**
-   * 사용자 정보 가져오기
-   */
-  public async memberinfo(): Promise<string> {
-    let resultpath: string;
-    await instance.get(ENDPOINT.API_MEMEBER).then((result) => {
-      let { data, message } = result.data;
-      // store 저장
-      data["islogin"] = true;
-      store.dispatch("auth/updateUserInfoInterface", data);
+			const role: string = data.role;
+			if (message == "success") {
+				resultpath = role === "SELLER" ? "admin/dashboard" : "commerce";
+			} else {
+				resultpath = "login";
+			}
+		});
+		return resultpath;
+	}
 
-      const role: string = data.role;
-      if (message == "success") {
-        resultpath = role === "SELLER" ? "admin/dashboard" : "commerce";
-      } else {
-        resultpath = "login";
-      }
-    });
-    return resultpath;
-  }
+	/**
+	 * 로그인
+	 * 액세스토큰 받기
+	 * 사용자 정보 가져오기
+	 */
+	public async memberlogin(payload: MemberLogin): Promise<string> {
+		// 액세스 토큰 받기
+		await this.loginccesstoken(payload);
 
-  /**
-   * 로그인
-   * 액세스토큰 받기
-   * 사용자 정보 가져오기
-   */
-  public async memberlogin(payload: MemberLogin): Promise<string> {
-    // 액세스 토큰 받기
-    await this.loginccesstoken(payload);
-
-    // 사용자 정보 가져오기(구매자/판매자에 따라 리다이렉팅 페이지가 다름)
-    const resultpath = await this.memberinfo();
-    return resultpath;
-  }
+		// 사용자 정보 가져오기(구매자/판매자에 따라 리다이렉팅 페이지가 다름)
+		const resultpath = await this.memberinfo();
+		return resultpath;
+	}
 }
 
 export default new UserAthendicateService();
