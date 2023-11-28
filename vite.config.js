47619b340d66ea296b152eef07b7be73b9fd7fc9
goeun23 <abgoeun3339@gmail.com>
2023-11-28 21:20:54 +0900

    빌드 해시맵 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-
+const hash = Math.floor(Math.random() * 90000) + 10000;
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
@@ -10,6 +10,14 @@ export default ({ mode }) => {
     root: "./",
     build: {
       outDir: "dist",
+
+      rollupOptions: {
+        output: {
+          entryFileNames: `[name]` + hash + `.js`,
+          chunkFileNames: `[name]` + hash + `.js`,
+          assetFileNames: `[name]` + hash + `.[ext]`,
+        },
+      },
     },
     //base: process.env.VITE_APP_BASE_URL,
 
